8ba37b15a5ce6978ede5774a1cae0330849a1c4e
kouheyHEY <vudtarm@gmail.com>
2023-08-24 00:06:08 +0900

    update js

--- mocks/js/01_scoreSimulator.js
+++ mocks/js/01_scoreSimulator.js
@@ -4,3 +4,15 @@
 function calcScore() {
 
 }
+
+/**
+ * レベルからレベルボーナスを計算する
+ */
+function calcLevelBonus() {
+    let bonusRate = 0;
+
+    if (level <= 10) {
+        bonusRate = level / 2;
+    } else if (level <= 20) {
+    }
+}
